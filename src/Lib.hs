dae1792581bb9269a74cf8449c6f6ec92b909259
Pedro Castilho <cstl.px@gmail.com>
2020-09-11 20:21:46 -0300

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,24 @@
+module Lib
+    ( someFunc
+    ) where
+
+data Term = Var String Int
+          | Abs Term
+          | App Term Term
+          deriving Show
+
+evalTerm :: [Term] -> Term -> Term
+evalTerm env (Var name index)
+  | index < 0 = (Var name index)
+  | otherwise = env !! index of
+evalTerm env (Abs body)  = Abs (evalTerm env body)
+evalTerm env (App (Abs body) term) =
+  let arg = evalTerm env term in
+    evalTerm (arg:env) body
+evalTerm env (App t1 t2) = App (evalTerm env t1) (evalTerm env t2)
+
+eval :: Term -> Term
+eval = evalTerm []
+
+main :: IO ()
+main = print $ eval (App (Abs (Var "x" 0)) (Var "x" (-1)))
